541a6f3f237d9f4ce153389b2e6dea822210a02c
zhoumeng0510 <1058166619@qq.com>
2017-05-16 17:00:59 +0800

    update

--- sql/day2.sql
+++ sql/day2.sql
@@ -1,8 +1,10 @@
-# DROP TABLE IF EXISTS db_1702;
-# CREATE DATABASE db_1702;
+# drop table if exists db_1702;
+# create database db_1702;
 
 DROP TABLE IF EXISTS db_1702.student;
 CREATE TABLE db_1702.student (
+  id     INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'id',
   name   VARCHAR(255) COMMENT '姓名',
   intro  TEXT COMMENT '简介',
   gender CHAR(2) COMMENT '性别',
@@ -13,7 +15,63 @@ CREATE TABLE db_1702.student (
   time   DATETIME COMMENT '时间'
 );
 
-INSERT INTO db_1702.student VALUE ('tom', 'yellow', 'm', 18, 1.75, 123.456, '1996-5-10', '2017-5-16 10:55:00');
+INSERT INTO db_1702.student VALUE (NULL, 'tom', 'yellow', 'm', 18, 1.75, 123.456, '1996-5-10', '2017-5-16 10:55:00');
 
 SELECT *
 FROM db_1702.student;
+
+DELETE FROM db_1702.student
+WHERE id > 1 AND id < 18;
+
+DELETE FROM db_1702.student;
+
+TRUNCATE TABLE db_1702.student; -- truncate  清空
+
+-- 课程表 课程名 学分 ...
+DROP TABLE IF EXISTS db_1702.course;
+CREATE TABLE db_1702.course (
+  id     INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID Pk',
+  title  VARCHAR(191) UNIQUE
+  COMMENT '课程名',
+  credit INT COMMENT '学分'
+);
+
+-- 选课表 学生<->课程
+DROP TABLE IF EXISTS db_1702.student_course;
+CREATE TABLE db_1702.student_course (
+  id        INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID PK',
+  studentId INT COMMENT '学生 ID',
+  courseId  INT COMMENT '课程 ID',
+  score     INT COMMENT '考试成绩'
+  --  PRIMARY KEY (studentId, courseId) -- 联合主键
+);
+
+SELECT *
+FROM db_1702.student;
+
+INSERT INTO db_1702.course VALUES (NULL, 'Java SE', 4);
+INSERT INTO db_1702.course VALUES (NULL, 'MySQL', 3);
+INSERT INTO db_1702.course VALUES (NULL, NULL, 3);
+INSERT INTO db_1702.course VALUES (NULL, NULL, 3);
+INSERT INTO db_1702.course VALUES (NULL, NULL, 3);
+
+SELECT *
+FROM db_1702.course;
+
+INSERT INTO db_1702.student_course VALUES (NULL, 2, 1, NULL);
+INSERT INTO db_1702.student_course VALUES (NULL, 2, 2, NULL);
+INSERT INTO db_1702.student_course VALUES (NULL, 1, 2, NULL);
+
+SELECT *
+FROM db_1702.student_course;
+
+UPDATE db_1702.student_course
+SET db_1702.student_course.score = 50
+WHERE id = 1;
+-- WHERE db_1702.student_course.courseId = 2 AND db_1702.student_course.studentId = 1;
+
+SHOW FULL COLUMNS FROM db_1702.student_course;
+
+SHOW VARIABLES LIKE 'char%';
---
+++ src/main/java/JDBC.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zhoumeng on
+ * 2017.5.16.
+ * 下午 04:46.
+ */
+public class JDBC {
+
+}
