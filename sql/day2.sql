8b92cb04627399a9debe5bd2b070f78b32795963
zhoumeng0510 <1058166619@qq.com>
2017-05-16 11:55:29 +0800

    update

--- sql/day2.sql
+++ sql/day2.sql
@@ -1,23 +1,19 @@
-drop table db_1702.student;
+# DROP TABLE IF EXISTS db_1702;
+# CREATE DATABASE db_1702;
 
-create table db_1702.student(
-  name varchar(255) COMMENT'姓名',
-  intro text COMMENT '简介',
-  gender char(2) COMMENT '性别',
-  age int unsigned COMMENT '年龄',
-  height double(3,2) COMMENT '身高',
-  price decimal(6,2)COMMENT '价格',
-  dob date COMMENT '出生日期',-- date of birth
-  time datetime COMMENT '时间'
+DROP TABLE IF EXISTS db_1702.student;
+CREATE TABLE db_1702.student (
+  name   VARCHAR(255) COMMENT '姓名',
+  intro  TEXT COMMENT '简介',
+  gender CHAR(2) COMMENT '性别',
+  age    INT UNSIGNED COMMENT '年龄',
+  height DOUBLE(3, 2) COMMENT '身高',
+  price  DECIMAL(6, 2) COMMENT '价格',
+  dob    DATE COMMENT '出生日期', -- date of birth
+  time   DATETIME COMMENT '时间'
 );
 
-SHOW FULL COLUMNS FROM db_1702.student;
+INSERT INTO db_1702.student VALUE ('tom', 'yellow', 'm', 18, 1.75, 123.456, '1996-5-10', '2017-5-16 10:55:00');
 
-insert into db_1702.student value ('tom','yellow','m',18,1.75,123.456,'1996-5-10','2017-5-16 10:55:00');
-
-TRUNCATE TABLE db_1702.student;
-
-DELETE FROM db_1702.student;
-
-select *
-from db_1702.student;
+SELECT *
+FROM db_1702.student;
